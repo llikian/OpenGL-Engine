d200bc7bbd9ecd2ff9d5c040a1483cb90c3ae366
llikian <pokillian64@gmail.com>
2024-04-27 17:16:20 +0200

    Removed the key callback bc it was buggy

--- include/callbacks.hpp
+++ include/callbacks.hpp
@@ -26,24 +26,6 @@ void windowSizeCallback(GLFWwindow* window, int width, int height);
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
 
 /**
- * @brief Callback for when a key is pressed, released or held down.
- * @param window The GLFW window.
- * @param key The affected key. This is a platform-independent key code using GLFW_KEY_*.
- * @param scancode The system-specific scancode for the key.
- * @param action The action performed. Can be one of three values:\n
- * - GLFW_PRESS: The key was pressed.\n
- * - GLFW_RELEASE: The key was released.\n
- * - GLFW_REPEAT: The key was held down and is being repeatedly sent.
- * @param mods Bit field representing the modifier keys that were held down when the event occured.
- * It is a bitwise OR combination of the following flags:\n
- * - GLFW_MOD_SHIFT\n
- * - GLFW_MOD_CONTROL\n
- * - GLFW_MOD_ALT\n
- * - GLFW_MOD_SUPER\n
- */
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-
-/**
  * @brief Callback for when a button of the mouse is pressed or released.
  * @param window The GLFW window.
  * @param button The mouse button that was pressed or released. Can be one of three values:\n
--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -13,16 +13,12 @@ void frameBufferSizeCallback(GLFWwindow* /* window */, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void keyCallback(GLFWwindow* /* window */, int key, int /* scancode */, int action, int mods) {
-    app.handleKeyEvent(key, action, mods);
-}
-
 void mouseButtonCallback(GLFWwindow* /* window */, int button, int action, int mods) {
 
 }
 
 void cursorPositionCallback(GLFWwindow* /* window */, double xPos, double yPos) {
-
+    app.handleCursorPositionEvent(xPos, yPos);
 }
 
 void scrollCallback(GLFWwindow* /* window */, double xOffset, double yOffset) {
