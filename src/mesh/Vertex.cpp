e1847cfd6c5d16f389f28f3ae98ef3909ad253cd
llikian <pokillian64@gmail.com>
2024-04-29 13:04:27 +0200

    Removed Vertex class because it's now unused

--- src/mesh/Vertex.cpp
+++
@@ -1,9 +0,0 @@
-/***************************************************************************************************
- * @file  Vertex.cpp
- * @brief Implementation of the Vertex class
- **************************************************************************************************/
-
-#include "mesh/Vertex.hpp"
-
-Vertex::Vertex(const Point& position, const Vector& normal, const TexCoord& texCoord)
-    : position(position), normal(normal), texCoord(texCoord) { }
