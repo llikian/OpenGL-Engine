dffc0b2132f88ab45799d5c96eb6e2cc804ca899
llikian <pokillian64@gmail.com>
2024-12-11 11:15:17 +0100

    Removed unnecessary dereferences

--- src/engine/Callbacks.cpp
+++ src/engine/Callbacks.cpp
@@ -13,10 +13,10 @@ Callbacks::Callbacks() : windowSizeCallback(nullptr),
                          scrollCallback(nullptr) { }
 
 void Callbacks::applyCallbacks(GLFWwindow* window) {
-    if(windowSizeCallback) { glfwSetWindowSizeCallback(window, *windowSizeCallback); }
-    if(frameBufferSizeCallback) { glfwSetFramebufferSizeCallback(window, *frameBufferSizeCallback); }
-    if(keyCallback) { glfwSetKeyCallback(window, *keyCallback); }
-    if(mouseButtonCallback) { glfwSetMouseButtonCallback(window, *mouseButtonCallback); }
-    if(cursorPositionCallback) { glfwSetCursorPosCallback(window, *cursorPositionCallback); }
-    if(scrollCallback) { glfwSetScrollCallback(window, *scrollCallback); }
+    if(windowSizeCallback) { glfwSetWindowSizeCallback(window, windowSizeCallback); }
+    if(frameBufferSizeCallback) { glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback); }
+    if(keyCallback) { glfwSetKeyCallback(window, keyCallback); }
+    if(mouseButtonCallback) { glfwSetMouseButtonCallback(window, mouseButtonCallback); }
+    if(cursorPositionCallback) { glfwSetCursorPosCallback(window, cursorPositionCallback); }
+    if(scrollCallback) { glfwSetScrollCallback(window, scrollCallback); }
 }
