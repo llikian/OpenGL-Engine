9dc2fc93f1ab2af210731b463af3c328edb55e94
llikian <pokillian64@gmail.com>
2025-02-19 14:07:04 +0100

    Documentation

--- include/maths/mat4.hpp
+++ include/maths/mat4.hpp
@@ -14,12 +14,12 @@
 struct mat4 {
 public:
     /**
-     * @brief Constructs a mat4 with all components equal to 0.
+     * @brief Constructs a matrix with all components equal to 0.
      */
     mat4();
 
     /**
-     * @brief Constructs a mat4 with a specific value for each component.
+     * @brief Constructs a matrix with a specific value for each component.
      * @param v00, v01, v02, v03 The values of the components of the first row.
      * @param v10, v11, v12, v13 The values of the components of the second row.
      * @param v20, v21, v22, v23 The values of the components of the third row.
@@ -31,7 +31,7 @@ public:
          float v30, float v31, float v32, float v33);
 
     /**
-     * @brief Constructs a mat4 with a specific value for each component of the 3 first columns
+     * @brief Constructs a matrix with a specific value for each component of the 3 first columns
      * and rows. The fourth column and row are the same as the identity matrix.
      * @param v00, v01, v02 The values of the first three components of the first row.
      * @param v10, v11, v12 The values of the first three components of the second row.
@@ -42,23 +42,25 @@ public:
          float v20, float v21, float v22);
 
     /**
-     * @brief Constructs a mat4 which is the identity matrix multiplied by a scalar.
+     * @brief Constructs a matrix which is the identity matrix multiplied by a scalar.
      * Each value of the diagonal is equal to the scalar and the rest is zeros.
      * @param scalar The value for the components on the diagonal.
      */
     mat4(float scalar);
 
     /**
-     * @brief Accesses a row in the underlying 2D array of values.
+     * @brief Accesses an element of the matrix.
      * @param row The row's index.
-     * @return The row with the given index.
+     * @param column The column's index.
+     * @return A reference to the element.
      */
     float& operator ()(int row, int column);
 
     /**
-     * @brief Accesses a row in the underlying 2D array of values.
+     * @brief Accesses an element of the matrix.
      * @param row The row's index.
-     * @return The row with the given index.
+     * @param column The column's index.
+     * @return A const reference to the element.
      */
     const float& operator ()(int row, int column) const;
 
@@ -67,81 +69,111 @@ private:
 };
 
 /**
- * @brief Writes the components of the given mat4 to the output stream in the format:\n
- * "( v00 ; v01 ; v02 ; v03 )\\n"\n
- * "( v10 ; v11 ; v12 ; v13 )\\n"\n
- * "( v20 ; v21 ; v22 ; v23 )\\n"\n
- * "( v30 ; v31 ; v32 ; v33 )\\n"\n
+ * @brief Writes the components of the given matrix to the output stream in the format:\n
+ * ( v00 ; v01 ; v02 ; v03 )\n
+ * ( v10 ; v11 ; v12 ; v13 )\n
+ * ( v20 ; v21 ; v22 ; v23 )\n
+ * ( v30 ; v31 ; v32 ; v33 )\n
  * @param stream The output stream to write to.
- * @param Matrix The mat4 to write to the stream.
- * @return A reference to the output stream after writing the mat4.
+ * @param mat The matrix to write to the stream.
+ * @return A reference to the output stream after writing the matrix.
  */
 std::ostream& operator <<(std::ostream& stream, const mat4& mat);
 
+/**
+ * @brief Adds the second matrix's components to the first's.
+ * @param left The left operand. Will store the result.
+ * @param right The right operand.
+ */
 void operator +=(mat4& left, const mat4& right);
 
+/**
+ * @brief Subtracts the second matrix's components from the first's.
+ * @param left The left operand. Will store the result.
+ * @param right The right operand.
+ */
 void operator -=(mat4& left, const mat4& right);
 
+/**
+ * @brief Adds a scalar to all of a matrix's components.
+ * @param mat The matrix.
+ * @param scalar The scalar.
+ */
 void operator +=(mat4& mat, float scalar);
 
+/**
+ * @brief Subtracts all of a matrix's components by a scalar.
+ * @param mat The matrix.
+ * @param scalar The scalar.
+ */
 void operator -=(mat4& mat, float scalar);
 
+/**
+ * @brief Multiplies a scalar to all of a matrix's components.
+ * @param mat The matrix.
+ * @param scalar The scalar.
+ */
 void operator *=(mat4& mat, float scalar);
 
+/**
+ * @brief Divides all of a matrix's components by a scalar.
+ * @param mat The matrix.
+ * @param scalar The scalar.
+ */
 void operator /=(mat4& mat, float scalar);
 
-/** @brief Adds a mat4's components to another's.
+/** @brief Adds a matrix's components to another's.
  *  @param left The left operand.
  *  @param right The right operand.
- *  @return The component-wise sum of the two mat4.
+ *  @return The component-wise sum of the two matrix.
  */
 mat4 operator +(const mat4& left, const mat4& right);
 
-/** @brief Subtracts a mat4's components by another's.
+/** @brief Subtracts a matrix's components by another's.
  *  @param left The left operand.
  *  @param right The right operand.
- *  @return The component-wise subtraction of the first mat4 by the second.
+ *  @return The component-wise subtraction of the first matrix by the second.
  */
 mat4 operator -(const mat4& left, const mat4& right);
 
-/** @brief Multiplies a mat4 by another.
+/** @brief Multiplies a matrix by another.
  *  @param left The left operand.
  *  @param right The right operand.
- *  @return The product of the two mat4.
+ *  @return The product of the two matrix.
  */
 mat4 operator *(const mat4& left, const mat4& right);
 
-/** @brief Adds a scalar to each of a mat4's components.
- *  @param Matrix The mat4.
+/** @brief Adds a scalar to each of a matrix's components.
+ *  @param mat The matrix.
  *  @param scalar The scalar.
- *  @return The component-wise sum of a mat4 by a scalar.
+ *  @return The component-wise sum of a matrix by a scalar.
  */
 mat4 operator +(const mat4& mat, float scalar);
 
-/** @brief Subtracts each of a mat4's components by a scalar.
- *  @param Matrix The mat4.
+/** @brief Subtracts each of a matrix's components by a scalar.
+ *  @param mat The matrix.
  *  @param scalar The scalar.
- *  @return The component-wise subtraction of a mat4 by a scalar.
+ *  @return The component-wise subtraction of a matrix by a scalar.
  */
 mat4 operator -(const mat4& mat, float scalar);
 
-/** @brief Multiplies each of a mat4's components by a scalar.
- *  @param Matrix The mat4.
+/** @brief Multiplies each of a matrix's components by a scalar.
+ *  @param mat The matrix.
  *  @param scalar The scalar.
- *  @return The component-wise product of a mat4 by a scalar.
+ *  @return The component-wise product of a matrix by a scalar.
  */
 mat4 operator *(const mat4& mat, float scalar);
 
-/** @brief Multiplies each of a mat4's components by a scalar.
+/** @brief Multiplies each of a matrix's components by a scalar.
  *  @param scalar The scalar.
- *  @param Matrix The mat4.
- *  @return The component-wise product of a mat4 by a scalar.
+ *  @param mat The matrix.
+ *  @return The component-wise product of a matrix by a scalar.
  */
 mat4 operator *(float scalar, const mat4& mat);
 
-/** @brief Divides each of a mat4's components by a scalar.
- *  @param Matrix The mat4.
+/** @brief Divides each of a matrix's components by a scalar.
+ *  @param mat The matrix.
  *  @param scalar The scalar.
- *  @return The component-wise division of a mat4 by a scalar.
+ *  @return The component-wise division of a matrix by a scalar.
  */
 mat4 operator /(const mat4& mat, float scalar);
